cff880d97a83287ccfc3519a8f773dd2d2afb6e4
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2020-06-30 17:25:43 +0000

    fin todays

--- myproject/myapp/manager.py
+++ myproject/myapp/manager.py
@@ -6,6 +6,8 @@ import json
 from django.http import JsonResponse
 # Create your views here.
 
+
+# Récuperation des categories sur le site
 def cat_api(request):
 
     url = 'https://www.zalando.fr/accueil-homme/'
@@ -33,6 +35,7 @@ def cat_api(request):
         print('erreur statut',status_code)
     return JsonResponse(mydata, safe=False)
 
+# Fonction pour récuperer le lien des collections
 def collection_api(request):
     url = 'https://www.zalando.fr/accueil-homme/'
 
@@ -48,18 +51,72 @@ def collection_api(request):
         datas =json.loads(data_to_python_json)
         datas = datas['graphqlCache']
         mydata = []
+        mydatas = []
         for key in datas.keys(): 
             if "ern:collection" in key:
                 try:
                     contdata = datas[key]['data']['collection']
                     urlCollection = contdata['uri']
                     titre = contdata['title']
+                    
                     # print(titre)
                     mydata.append(urlCollection)
+                    print(urlCollection)
+                        
+                        # urldata = m
+                        # # print(urldata)
+                        # header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+                        # responses = get(urldata,headers=header)
+
+                        # html_soups = BeautifulSoup(responses.text, 'html.parser')
+
+                        # if responses.status_code == 200:
+                            
+                        #     div_module = html_soups.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
+                        #     data_to_python_json = div_module.contents[0].replaceWith("")
+                        #     datas =json.loads(data_to_python_json)
+                        #     datas = datas['graphqlCache']
+                            
+                        #     for key in datas.keys(): 
+                        #         if "ern:product" in key:
+                        #             try:
+                        #                 contdata = datas[key]['data']['product']
+                        #                 mydatas.append(contdata)
+                        #             except:
+                        #                 pass
+                        # else:
+                        #     print('erreur statut',status_code)
+                        # return JsonResponse(mydatas, safe=False)
+                except Exception as e:
+                    print(str(e))
+    else:
+        print('erreur statut',response.status_code)
+    return JsonResponse(mydatas, safe=False)
+
+## Fonctions pour récuperer les données d'une collection
+def data_collection(request):
+    
+    url = 'https://www.zalando.fr/collections/cIjAWB_QQ6q/'
+
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    response = get(url,headers=headers)
+
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    if response.status_code == 200:
+        
+        div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
+        data_to_python_json = div_module.contents[0].replaceWith("")
+        datas =json.loads(data_to_python_json)
+        datas = datas['graphqlCache']
+        mydata = []
+        for key in datas.keys(): 
+            if "ern:product" in key:
+                try:
+                    contdata = datas[key]['data']['product']
+                    mydata.append(contdata)
                 except:
                     pass
     else:
-        print('erreur statut',response.status_code)
+        print('erreur statut',status_code)
     return JsonResponse(mydata, safe=False)
-    
-    
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .manager import cat_api, collection_api
+from .manager import cat_api, collection_api, data_collection
 
 app_name = 'myapp'
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/', views.json_api, name='api'),
     path('categorie', cat_api, name='categorie'),
     path('collection', collection_api, name='collection'),
+    path('collection-data', data_collection, name='collection_data'),
     path('apimodehomme/', views.json_mode_homme, name='apimodehomme'),
 
 ]
