1ffa6e7020e8197d33ecf279982de2ddb6e4d8f5
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2020-06-30 15:56:08 +0000

    lien collections

--- myproject/myapp/manager.py
+++ myproject/myapp/manager.py
@@ -32,5 +32,34 @@ def cat_api(request):
     else:
         print('erreur statut',status_code)
     return JsonResponse(mydata, safe=False)
+
+def collection_api(request):
+    url = 'https://www.zalando.fr/accueil-homme/'
+
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    response = get(url,headers=headers)
+
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    if response.status_code == 200:
+        div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
+        data_to_python_json = div_module.contents[0].replaceWith("")
+        
+        datas =json.loads(data_to_python_json)
+        datas = datas['graphqlCache']
+        mydata = []
+        for key in datas.keys(): 
+            if "ern:collection" in key:
+                try:
+                    contdata = datas[key]['data']['collection']
+                    urlCollection = contdata['uri']
+                    titre = contdata['title']
+                    # print(titre)
+                    mydata.append(urlCollection)
+                except:
+                    pass
+    else:
+        print('erreur statut',response.status_code)
+    return JsonResponse(mydata, safe=False)
     
     
