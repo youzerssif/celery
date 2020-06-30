442488f260a701ba2f012b72afc90e815e0d4b8b
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2020-06-30 14:22:43 +0000

    recuperation des categorie d'hommes

---
+++ myproject/myapp/manager.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import json
+from django.http import JsonResponse
+# Create your views here.
+
+def cat_api(request):
+
+    url = 'https://www.zalando.fr/accueil-homme/'
+
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    response = get(url,headers=headers)
+
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    if response.status_code == 200:
+        
+        div_module = html_soup.find('script', attrs = {'id':'z-navicat-header-props'}, type='application/json')
+        data_to_python_json = div_module.contents[0].replaceWith("")
+        
+        d = data_to_python_json.replace('<![CDATA[', '')
+        de = d.replace(']]>', '')
+        datas = json.loads(de)
+        
+        # print()
+        # print(de)
+        c = datas['model']['topnavi']['children'][2]['children']
+        
+        print(c)
+        mydata = []
+        mydata.append(c)
+    else:
+        print('erreur statut',status_code)
+    return JsonResponse(mydata, safe=False)
+    
+    
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from .manager import cat_api
 
 app_name = 'myapp'
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/', views.json_api, name='api'),
+    path('categorie', cat_api, name='categorie')
 
 ]
