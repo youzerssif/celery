4aa34b1f432ad4a99e13b285de8d026cb8cd2de1
youzerssif <samakeyouzerssif@gmail.com>
2020-07-01 16:01:33 +0000

    bien structure le code

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -56,8 +56,6 @@ def json_api(request):
 
     if response.status_code == 200:
         
-        # print('okkokokookoko')
-        # print(html_soup.prettify())
         
         div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
         # data = json.loads(div_module.text)
@@ -80,25 +78,6 @@ def json_api(request):
                 except:
                     pass
        
-
-        
-        # module = div_module.find_all('div', attrs = {'class':'col-sm-6 col-p10'})
-                
-        # for item in div_module:
-        #     #------------RECUPERATION---------------#  
-        #     print(item)      
-            # div = item.find('div', attrs = {'class':'col-sm-8 col-8 col-per'})
-            # img = item.find('div', attrs = {'class':'col-sm-4 col-4'})
-            # #------------AFFECTATION---------------# 
-            # a = div.find('a')
-            # serv = div.find('span').get_text()
-            # imag = img.find('img')
-            # image = imag['src']
-            # lien = a['href']
-            # #------------AFFICHAGE---------------#
-            # print(lien)
-            # print(image)
-            # print(serv)
     else:
         print('erreur statut',response.status_code)
     return JsonResponse(mydata, safe=False)
@@ -106,73 +85,7 @@ def json_api(request):
 ######## elle recupere les datas des pages categories ##############  
 def json_mode_homme(request):
 
-    try:
-        url = 'https://www.zalando.fr/promo-homme/'
-
-        headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-        response = get(url,headers=headers)
-
-        html_soup = BeautifulSoup(response.text, 'html.parser')
-
-        if response.status_code == 200:
-            
-            # print('okkokokookoko')
-            # print(html_soup.prettify())
-            
-            div_module = html_soup.find('script', id ="z-nvg-cognac-props", type='application/json')
-            # data = json.loads(div_module.text)
-            data_to_python_json = div_module.contents[0].replaceWith("")
-            # print(data,"zjncldncze")
-
-            data = json.loads(data_to_python_json.strip('<![CDATA[').strip(']>'))
-            data = data['articles']
-    except Exception as e:
-        print(str(e))
-        
-        
-    # url = 'https://www.zalando.fr/accueil-luxe-homme/'
-
-    # headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-    # response = get(url,headers=headers)
-
-    # html_soup = BeautifulSoup(response.text, 'html.parser')
-
-    # if response.status_code == 200:
-        
-    #     # print('okkokokookoko')
-    #     # print(html_soup.prettify())
-        
-    #     div_module = html_soup.find('script', id ="app-props", type='application/json')
-    #     # data = json.loads(div_module.text)
-    #     data_to_python_json = div_module.contents[0].replaceWith("")
-    #     # print(data_to_python_json.replace('<![CDATA[','').replace(']>',''),"zjncldncze")
-
-    #     data = json.loads(data_to_python_json.replace('<![CDATA[','').replace(']]>',''))
-    #     mydata = []
-    #     # print(data)
-    #     for item in data['content']['brands']['brands']:
-    #         mydata.append(item)
-            # print(item)
-        # data = data['articles']
-        # data = datas['graphqlCache']
-                   
-        # mydata = []
-        # for key in data.keys(): 
-        #     print(key)
-            # if "ern:product" in key:
-            #     try:
-            #         contdata = datas[key]['data']['product']
-            #         # print(contdata)
-            #         mydata.append(contdata)
-                    
-            #     except:
-        #         pass
-
-    else:
-        print('erreur statut',response.status_code)
-    return JsonResponse(data, safe=False)
-def json_detail_article(request): 
-    url = 'https://www.zalando.fr/lindbergh-plain-mens-suit-slim-fit-costume-lg522a002-o13.html'
+    url = 'https://www.zalando.fr/accueil-luxe-homme/'
 
     headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
     response = get(url,headers=headers)
@@ -180,32 +93,75 @@ def json_detail_article(request):
     html_soup = BeautifulSoup(response.text, 'html.parser')
 
     if response.status_code == 200:
-        
-        # print('okkokokookoko')
-        # print(html_soup.prettify())
-        
-        div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
-        # data = json.loads(div_module.text)
-        data_to_python_json = div_module.contents[0].replaceWith("")
-        # print(data,"zjncldncze")
+            
+        try:
+                
+            div_module = html_soup.find('script', id ="app-props", type='application/json')
+            # data = json.loads(div_module.text)
+            data_to_python_json = div_module.contents[0].replaceWith("")
+            # print(data_to_python_json.replace('<![CDATA[','').replace(']>',''),"zjncldncze")
 
-        datas =json.loads(data_to_python_json)
-        datas = datas['graphqlCache']
-        
-        # print()
-        # print(datas)
-        mydata = []
-        for key in datas.keys(): 
-            if "ern:product" in key:
-                try:
-                    contdata = datas[key]['data']['product']
-                    # print(contdata)
-                    mydata.append(contdata)
+            data = json.loads(data_to_python_json.replace('<![CDATA[','').replace(']]>',''))
+            mydata = []
+            # print(data)
+            for item in data['content']['brands']['brands']:
+                mydata.append(item)
+                # print(item)
+            # data = data['articles']
+
                     
-                except:
-                    pass
+        except Exception as e:
+            print(str(e))
+
+    else:
+        print('erreur statut',response.status_code)
+    return JsonResponse(mydata, safe=False)
+
+
+def json_detail_article(request):
+    
+    url = 'https://www.zalando.fr/lindbergh-plain-mens-suit-slim-fit-costume-lg522a002-o13.html'
+
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    response = get(url,headers=headers)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    if response.status_code == 200:
+        
+        try:
+            div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
+            data_to_python_json = div_module.contents[0].replaceWith("")
+            datas =json.loads(data_to_python_json)
+            datas = datas['graphqlCache']
+        except Exception as e:
+            print(str(e))
+        
+        try:
+            div_module = html_soup.find('script', id ="z-vegas-shipping-props", type='application/json')
+            # data = json.loads(div_module.text)
+            data_to_python_json = div_module.contents[0].replaceWith("")
+
+            
+            
+            
+            articleInfo = json.loads(data_to_python_json.strip('<![CDATA[').strip(']>'))
+            
+
+            mydata = []
+            for key in datas.keys(): 
+                if "ern:product" in key:
+                    try:
+                        contdata = datas[key]['data']['product']
+                        # print(contdata)
+                        mydata.append(contdata)
+                        
+                    except:
+                        pass
+                        
+        except Exception as e:
+            print(str(e)) 
         
         
     else:
         print('erreur statut',response.status_code)
-    return JsonResponse(mydata, safe=False)
+    return JsonResponse(articleInfo, safe=False)
