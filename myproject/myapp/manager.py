2af38c89d6e65833a488dc75e8c121c55f260af4
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2020-07-02 17:32:39 +0000

    avance

--- myproject/myapp/manager.py
+++ myproject/myapp/manager.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 
 
 # Récuperation des categories sur le site
-def cat_api():
+def cat_api(request):
 
     url = 'https://www.zalando.fr/accueil-homme/'
 
@@ -34,26 +34,52 @@ def cat_api():
         # print(mydata)
     else:
         print('erreur statut',status_code)
-    return mydata
+    return JsonResponse(data=mydata, safe=False) 
 
-# class SrcpCollection:
-#     def __init__(self):
-#         super().__init__()
-        
-#     def getUrl(self):
-#         url = 'https://www.zalando.fr/accueil-homme/'
-#         headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-        
-#         req = requests.get(url,headers=headers)
-#         print("redirect",req.url, req.is_redirect)
-#         status = False
-#         soup = ""
-#         if req.status_code == 200:
-#             html_doc = req.text
-#             soup = BeautifulSoup(html_doc, 'html.parser')
-#             status = True
-#         return status, soup
-
+def look(request):
+    url = 'https://www.zalando.fr/get-the-look-homme/'
+    # https://www.zalando.fr/get-the-look-homme/?styleFilter=style_classic
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    
+    req = requests.get(url,headers=headers)
+    # print("redirect",req.url, req.is_redirect)
+    status = False
+    soup = ""
+    cnt = 0
+    data = []
+    if req.status_code == 200:
+        html_doc = req.text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        try:
+            for x in soup.find_all('div'):
+                myattrs = x.attrs
+                # print(x)
+                if 'onclick' in myattrs.keys():
+                    data = x
+                    # d = datas.contents[0].replaceWith("")
+                    # data = json.loads(d)
+                    # print(datas)
+                    cnt += 1
+                    datas = myattrs['onclick'].strip('return')
+                    # print(type(datas))
+                    d = json.loads(datas)
+                    menuLook = d["props"]["filters"][0]['options']
+                    for m in menuLook:
+                        # Recuperation des liens de categorie de look
+                        allStyle = m['label']
+                        # print(allStyle)
+                        urlda = "https://www.zalando.fr/get-the-look-homme/?styleFilter={}".format(allStyle)
+                        print(urlda)
+                        
+                    
+                    # cle = d["props"]["entityPositionsByFiltersHash"]
+                    # print(d)
+                    # data.append(d)
+                    break
+        except:
+            data = None
+        status = True
+    return JsonResponse(data=d, safe=False)
 # Fonction pour récuperer le lien des collections
 def collection_api(request):
     url = 'https://www.zalando.fr/accueil-homme/'
@@ -81,39 +107,39 @@ def collection_api(request):
                         # print("ok")
                         url_valide =  contdata['uri']
                         # mydata.append(url_valide)
-                        print(url_valide)
-                        url = url_valide
-                        # print(url)
+                    #     print(url_valide)
+                    #     url = url_valide
+                    #     # print(url)
 
-                        headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-                        response = get(url,headers=headers)
+                    #     headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+                    #     response = get(url,headers=headers)
 
-                        html_soup = BeautifulSoup(response.text, 'html.parser')
+                    #     html_soup = BeautifulSoup(response.text, 'html.parser')
 
-                        if response.status_code == 200:
+                    #     if response.status_code == 200:
                             
-                            div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
-                            data_to_python_json = div_module.contents[0].replaceWith("")
-                            datas =json.loads(data_to_python_json)
-                            datas = datas['graphqlCache']
-                            mydata = []
-                            for key in datas.keys(): 
-                                if "ern:product" in key:
-                                    try:
-                                        contdata = datas[key]['data']['product']
-                                        # print(contdata)
-                                        mydata.append(contdata)
-                                    except:
-                                        pass
-                        else:
-                            print('erreur statut',status_code)
-                        return JsonResponse(data=mydata, safe=False)
-                    elif not "https://www.zalando.fr/collections/" in contdata['uri']:
-                        print('les autres liens')
-                        other = contdata['uri']
-                    else:
-                        pass
-                    # urlCollections = urlCollection
+                    #         div_module = html_soup.find('script', attrs = {'class':'re-data-el-hydrate'}, type='application/json')
+                    #         data_to_python_json = div_module.contents[0].replaceWith("")
+                    #         datas =json.loads(data_to_python_json)
+                    #         datas = datas['graphqlCache']
+                    #         mydata = []
+                    #         for key in datas.keys(): 
+                    #             if "ern:product" in key:
+                    #                 try:
+                    #                     contdata = datas[key]['data']['product']
+                    #                     # print(contdata)
+                    #                     mydata.append(contdata)
+                    #                 except:
+                    #                     pass
+                    #     else:
+                    #         print('erreur statut',status_code)
+                    #     return JsonResponse(data=mydata, safe=False)
+                    # elif not "https://www.zalando.fr/collections/" in contdata['uri']:
+                    #     print('les autres liens')
+                    #     other = contdata['uri']
+                    # else:
+                    #     pass
+                    # # urlCollections = urlCollection
                 except Exception as e:
                     print(str(e))
     else:
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .manager import cat_api, collection_api, data_collection
+from .manager import cat_api, collection_api, data_collection, look
 
 app_name = 'myapp'
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/', views.json_api, name='api'),
     path('categorie', cat_api, name='categorie'),
     path('collection', collection_api, name='collection'),
+    path('look', look, name='look'),
     path('collection-data', data_collection, name='collection_data'),
     path('apimodehomme/', views.json_mode_homme, name='apimodehomme'),
     path('json_detail_article/', views.json_detail_article, name='json_detail_article'),
