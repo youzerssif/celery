0b1ccef40997615d45216663b01a588d1467c5c5
DoumbiaMamoutou <47214761+DoumbiaMamoutou@users.noreply.github.com>
2020-07-06 14:39:36 +0000

    admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -66,6 +66,36 @@ class SousCategorieAdmin(admin.ModelAdmin):
 
     desactive.short_description = "desactivés Les SousCategories selectionnées"
 
+class SousCategorieItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'categorie',
+        'nom',
+        'statut',
+        'date_add'
+    )
+    list_filter = (
+        'statut',
+        'date_add'
+    )
+    
+    search_fields = ('nom',)
+    date_hierarchy = ('date_add')
+    actions = ('active', 'desactive')
+    
+    list_per_page = 10
+
+    def active(self, request, queryset):
+        queryset.update(statut=True)
+        self.message_user(request, "La selection a été activé avec succés")
+
+    active.short_description = "activer Les SousCategories selectionnées"
+
+    def desactive(self, request, queryset):
+        queryset.update(statut=False)
+        self.message_user(request, "La selection a été desactivé avec succés")
+
+    desactive.short_description = "desactivés Les SousCategories selectionnées"
+
 class ProduitAdmin(admin.ModelAdmin, DynamicArrayMixin):
     list_display = (
         'sous_categorie_item',
@@ -171,7 +201,8 @@ def _register(model, admin_class):
 
 
 _register(models.Produit, ProduitAdmin)
-# _register(models.Look, LookAdmin)
+_register(models.Look, LookAdmin)
+_register(models.SousCategorieItem, SousCategorieItemAdmin)
 _register(models.Collection, CollectionAdmin)
 _register(models.Categorie, CategorieAdmin)
 _register(models.SousCategorie, SousCategorieAdmin)
