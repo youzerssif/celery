51e95f9e47763759f59f2f783b1d689b30ea931a
youzerssif <samakeyouzerssif@gmail.com>
2020-07-06 13:02:33 +0000

    connecte postgresql

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -83,10 +83,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'entre_homme',
+        'USER': 'postgres',
+        'PASSWORD': 'samakenan',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
+
 }
 
 
